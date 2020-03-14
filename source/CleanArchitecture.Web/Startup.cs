ee13ddfa7c2f3d61319a69affaf4b6162d7b4108
Mehtap <ayzdru@gmail.com>
2020-03-14 19:53:36 +0300

    Source klasörü oluşturuldu.

--- source/CleanArchitecture.Web/Startup.cs
+++ source/CleanArchitecture.Web/Startup.cs
@@ -3,26 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using CleanArchitecture.Infrastructure.Data;
+using MediatR;
+using CleanArchitecture.Web.Behaviours;
+using System.Reflection;
+using CleanArchitecture.Core.Entities;
+using Microsoft.AspNetCore.Http;
 
 namespace CleanArchitecture.Web
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public IWebHostEnvironment  WebHostEnvironment { get; }
+        public Startup(IConfiguration configuration, IWebHostEnvironment  webHostEnvironment)
         {
             Configuration = configuration;
+            WebHostEnvironment = webHostEnvironment;
         }
 
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
+    
         public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddRazorPages();
+        {            
+            services.AddResponseCompression();
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddIdentity<ApplicationUser, ApplicationRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();            
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestPerformanceBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestValidationBehavior<,>));
+            var mvcBuilder = services.AddRazorPages();
+#if DEBUG
+            if (WebHostEnvironment.IsDevelopment())
+            {
+                mvcBuilder.AddRazorRuntimeCompilation();
+            }
+#endif
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,16 +57,18 @@ namespace CleanArchitecture.Web
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
             }
             else
             {
-                app.UseExceptionHandler("/Error");
+                app.UseExceptionHandler("/Error");              
             }
 
             app.UseStaticFiles();
-
+            app.UseResponseCompression();
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
--- source/CleanArchitecture.Web/wwwroot/js/site.js
+++ source/CleanArchitecture.Web/wwwroot/js/site.js
@@ -1,4 +1,62 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function showAlert(title, message, type) {
+    $.notifyClose();
+    $.notify({
+        title: '<strong>' + title + '</strong>',
+        message: '</br>' + message
+    },
+        {
+            type: type,
+            newest_on_top: true,
+            z_index: 1051
+        }
+    );
+}
+function successAlert(data) {
+    if (data) {5
+        showAlert(data.title, data.message, 'success');
+    }
+}
+function errorAlert(error) {
+    if (error) {
+        showAlert(error.title, error.message, 'danger');
+    }
+}
+function formSubmit(form, modalId, onSuccess, hideAlert) {
+    hideAlert = typeof hideAlert !== 'undefined' ? hideAlert : false;
+    var $form = $(form);
+    if ($form.valid()) {
+        var $modal = $('#' + modalId);
+        var $button = $form.find('button[type=submit]');
+        var formData = new FormData();
+        var formParams = $form.serializeArray();
+        $.each($form.find('input[type="file"]'), function (i, tag) {
+            $.each($(tag)[0].files, function (i, file) {
+                formData.append(tag.name, file);
+            });
+        });
 
-// Write your Javascript code.
+        $.each(formParams, function (i, val) {
+            formData.append(val.name, val.value);
+        });
+        $.ajax({
+            url: $form.prop('action'),
+            data: formData,
+            cache: false,
+            contentType: false,
+            processData: false,
+            type: 'POST',
+            success: function (data, textStatus, jqXHR) {
+                $modal.modal('hide');
+                if (hideAlert == false) {
+                    successAlert(data);
+                }               
+                if (onSuccess) {
+                    onSuccess(data);
+                }
+            },
+            error: function (error) {              
+                errorAlert(error.responseJSON);               
+            }
+        });        
+    }
+}
